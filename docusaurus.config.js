b49528f7e266f26b2560dcf5a5e85f665b24f890
SholaOti <shola.otitoju2018@gmail.com>
2022-09-25 17:44:34 -0400

    updates

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -1,8 +1,5 @@
 const lightCodeTheme = require("prism-react-renderer/themes/github");
 const darkCodeTheme = require("prism-react-renderer/themes/dracula");
-const math = require("remark-math");
-const katex = require("rehype-katex");
-
 // With JSDoc @type annotations, IDEs can provide config autocompletion
 /** @type {import('@docusaurus/types').DocusaurusConfig} */
 (
