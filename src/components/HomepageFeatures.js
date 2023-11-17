0767a472a26a35de20de2d085280ba272f22d94c
Shola <shola.otitoju2018@gmail.com>
2023-11-17 14:12:27 -0500

    Updated Homepage

--- src/components/HomepageFeatures.js
+++ src/components/HomepageFeatures.js
@@ -9,7 +9,7 @@ const FeatureList = [
     description: (
       <>
         Docusaurus was designed from the ground up to be easily installed and
-        used to get your website up and running quickly.
+        used to get your website up and running quickly and fast.
       </>
     ),
   },
